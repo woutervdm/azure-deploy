1b03213201c291888426bfdf4fc05931c4496719
Wouter van de Molengraft <wouter@softmedia.nl>
2020-08-20 12:52:44 +0200

    updated azure library to @azure version

--- index.js
+++ index.js
@@ -2,16 +2,14 @@
 
 require('dotenv').config();
 
-const azure = require('azure-storage');
+const { BlobServiceClient } = require('@azure/storage-blob');
 const glob = require('glob-promise');
 const _ = require('lodash');
-const moment = require('moment');
+const dayjs = require('dayjs');
 const util = require('util');
-const fs = require('fs');
+const { createReadStream, ...fs } = require('fs');
 
 const stat = util.promisify(fs.stat);
-
-const blobService = azure.createBlobService();
 
 const containerName = '$web';
 const source = process.argv[2];
@@ -22,60 +20,38 @@ if (!source) {
 }
 
 if (!process.env.AZURE_STORAGE_CONNECTION_STRING) {
-  console.error('Please set the environment variable AZURE_STORAGE_CONNECTION_STRING to an appropriate value');
+  console.error(
+    'Please set the environment variable AZURE_STORAGE_CONNECTION_STRING to an appropriate value');
   process.exit(2);
 }
 
-const list = continuationToken => new Promise((accept, reject) => {
-  blobService.listBlobsSegmented(containerName, continuationToken, (err, data) => {
-    if (err) {
-      reject(err);
-    } else {
-      accept(data);
-    }
-  });
-});
+const blobService = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+const containerClient = blobService.getContainerClient(containerName);
 
-const listAll = async () => {
-  let result = [];
+async function listAll() {
+  const result = [];
 
-  let response = null;
-
-  do {
-    response = await list(response ? response.continuationToken : null);
-    result = result.concat(response.entries);
-  } while(response.continuationToken);
+  const iter = containerClient.listBlobsFlat();
+  for await (let item of iter) {
+    result.push(item);
+  }
 
   return result;
-};
+}
 
-const upload = async (source, dest) => new Promise((accept, reject) => {
-  blobService.createBlockBlobFromLocalFile(containerName, dest, source, {
-    .../\.(js|css|woff|ttf|png|jpg|svg)$/.exec(dest) ? {contentSettings: {
-      cacheControl: 'public,max-age=31536000'
-    }} : {}
-  }, err => {
-    if (err) {
-      reject(err);
-    }
-    else {
-      accept();
-    }
-  })
-});
+async function upload(source, dest) {
+  const blockBlobClient =  containerClient.getBlockBlobClient(dest);
+  if (/\.(js|css|woff|ttf|png|jpg|svg|ico)$/.exec(dest)) {
+    await blockBlobClient.setHTTPHeaders({
+      blobCacheControl: 'public,max-age=31536000'
+    });
+  }
 
-const remove = async blobName => new Promise((accept, reject) => {
-  blobService.deleteBlobIfExists(containerName, blobName, err => {
-    if (err) {
-      reject(err);
-    } else {
-      accept();
-    }
-  });
-});
+  return blockBlobClient.uploadFile(source);
+}
 
 (async () => {
-  const files = await glob('**', {cwd: source, nodir: true});
+  const files = await glob('**', { cwd: source, nodir: true });
 
   const entries = await listAll();
 
@@ -83,29 +59,37 @@ const remove = async blobName => new Promise((accept, reject) => {
   const entryByName = _.keyBy(entries, 'name');
 
   const newFiles = _.difference(files, entryNames);
+  /**
+   * @type {string[]}
+   */
   const deletedFiles = _.difference(entryNames, files);
 
   const toCheck = _.intersection(files, entryNames);
 
   // perform date check on toCheck
   const newerFiles = [];
-  for(let entry of toCheck) {
-    const {mtime} = await stat(`${source}/${entry}`);
+  for (let entry of toCheck) {
+    const { mtime } = await stat(`${source}/${entry}`);
 
-    const lastModified = moment(entryByName[entry].lastModified);
-    if (lastModified < moment(mtime)) {
+    const lastModified = dayjs(entryByName[entry].lastModified);
+    if (lastModified < dayjs(mtime)) {
       newerFiles.push(entry);
     }
   }
 
-  for(let entry of newFiles.concat(newerFiles)) {
+  for (let entry of newFiles.concat(newerFiles)) {
     console.log(`Uploading ${entry}`);
     await upload(`${source}/${entry}`, entry);
   }
 
-  for(let entry of deletedFiles) {
+  for (let entry of deletedFiles) {
     console.log(`Deleting ${entry}`);
-    await remove(entry);
+  }
+
+  for(let i=0; i<deletedFiles.length; i+=256) {
+    await blobService.getBlobBatchClient().deleteBlobs(
+      deletedFiles.slice(i, i+256).map(file => containerClient.getBlobClient(file))
+    );
   }
 })().catch(e => {
   console.error(e);
